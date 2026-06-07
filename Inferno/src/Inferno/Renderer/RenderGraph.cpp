7f38420a20ffaeacdb3a64fd7cd33bc168eda9f0
Danny Woods <dannyeatsoats29@gmail.com>
2026-06-07 18:32:21 +0200

    Started RenderGraph Compile

--- Inferno/src/Inferno/Renderer/RenderGraph.cpp
+++ Inferno/src/Inferno/Renderer/RenderGraph.cpp
@@ -1,5 +1,7 @@
 #include <pch.h>
+#include <stdexcept>
 #include <utility>
+#include <vector>
 
 #include "RenderGraph.h"
 
@@ -32,5 +34,55 @@ void RenderGraph::AddPass(const std::string &name,
   m_Passes.emplace_back(std::move(pass));
 }
 
-void RenderGraph::Compile() {}
+void RenderGraph::Compile() {
+  std::vector<std::vector<size_t>> dependencies(m_Passes.size());
+  std::vector<std::vector<size_t>> dependents(m_Passes.size());
+
+  std::unordered_map<std::string, size_t> resourceWriters;
+
+  for (size_t i = 0; i < m_Passes.size(); ++i) {
+    const auto &pass = m_Passes[i];
+
+    for (const auto &input : pass.Inputs) {
+      auto it = resourceWriters.find(input);
+      if (it != resourceWriters.end()) {
+        dependencies[i].push_back(it->second);
+        dependents[it->second].push_back(i);
+      }
+    }
+
+    for (const auto &output : pass.Outputs) {
+      resourceWriters[output] = i;
+    }
+  }
+
+  std::vector<bool> visited(m_Passes.size(), false);
+  std::vector<bool> inStack(m_Passes.size(), false);
+
+  std::function<void(size_t)> visit = [&](size_t node) {
+    if (inStack[node]) {
+      throw std::runtime_error("Cycle detected in RenderGrpah!");
+    }
+
+    if (visited[node]) {
+      return;
+    }
+
+    inStack[node] = true;
+
+    for (auto dependent : dependents[node]) {
+      visit(dependent);
+    }
+
+    inStack[node] = false;
+    visited[node] = true;
+    m_ExecutionOrder.push_back(node);
+  };
+
+  for (size_t i = 0; i < m_Passes.size(); ++i) {
+    if (!visited[i]) {
+      visit(i);
+    }
+  }
+}
 } // namespace Inferno
